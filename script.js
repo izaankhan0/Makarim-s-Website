d88061985c93d20bbfcadf0065eebda22a359cfa
Izaan Khan <izaankhan.x@gmail.com>
2023-12-24 12:19:35 +0500

    Add animations

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
       showReview((currentReviewIndex + 1) % reviews.length);
     }
   
-    setInterval(autoChangeReview, 11000);
+    setInterval(autoChangeReview, 15000);
   
     dots.forEach((dot, index) => {
       dot.addEventListener('click', function() {
@@ -37,3 +37,106 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+// JavaScript
+const menuIcon = document.querySelector('.menu-icon');
+const hiddenMenu = document.querySelector('.hidden-menu');
+const body = document.body;
+
+menuIcon.addEventListener('click', function(event) {
+  hiddenMenu.classList.toggle('show-menu');
+  body.classList.toggle('no-scroll');
+  event.stopPropagation();
+});
+
+document.addEventListener('click', function(event) {
+  const isClickInsideMenu = hiddenMenu.contains(event.target);
+  if (!isClickInsideMenu && hiddenMenu.classList.contains('show-menu')) {
+    toggleMenu();
+  }
+});
+
+const closeButtons = document.querySelectorAll('.close');
+
+closeButtons.forEach(button => {
+  button.addEventListener('click', function(event) {
+    toggleMenu();
+    event.stopPropagation();
+  });
+});
+
+function toggleMenu() {
+  hiddenMenu.classList.toggle('show-menu');
+  body.classList.toggle('no-scroll');
+}
+
+
+// Your existing smooth scroll logic
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+
+      // Simulate a click in the middle of the screen
+      const clickElement = document.createElement('div');
+      clickElement.style.position = 'fixed';
+      clickElement.style.width = '1px';
+      clickElement.style.height = '1px';
+      clickElement.style.top = '50%';
+      clickElement.style.left = '50%';
+      document.body.appendChild(clickElement);
+
+      clickElement.click(); // Trigger the click event
+      document.body.removeChild(clickElement); // Remove the temporary element
+    }
+  });
+});
+
+
+
+function reloadPage() {
+  location.reload();
+}
+
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // This enables smooth scrolling
+  });
+}
+
+
+
+const loader = document.querySelector('#preloader')
+window.addEventListener('load',function(){
+  loader.style.display="none"
+})
+
+
+
+
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+  const reveals = document.querySelectorAll('.jam')
+
+  for (i = 0; i < reveals.length; i++){
+    const winHeight = window.innerHeight;
+    const revealtop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealtop < winHeight - revealPoint){
+      reveals[i].classList.add('jam-active')
+    } else{
+      reveals[i].classList.remove('jam-active')
+    }
+  }
+}
+
